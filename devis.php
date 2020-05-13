9604dd3a883a66eefe1387f6f635a85d4453e5db
Eric <ec83fr@yahoo.fr>
2020-05-13 17:44:46 +0200

    base page devis

--- devis.php
+++ devis.php
@@ -1,3 +1,80 @@
+<?php
+require_once('DevWebPeinture_db.php'); 
+
+$DevWebPeinture_db = mysqli_connect($hostname_DevWebPeinture_db, $username_DevWebPeinture_db, $password_DevWebPeinture_db, $database_DevWebPeinture_db);
+$erreur=mysqli_connect_errno();
+if ($erreur<>0) {echo "echec lors de la connexion: ".$erreur;} 
+echo mysqli_connect_error();
+
+if (!function_exists("GetSQLValueString")) 
+{
+  function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+	{
+	  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+	  switch ($theType) 
+	   {
+		case "text":
+		  $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+		  break;    
+		case "long":
+		case "int":
+		  $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+		  break;
+		case "double":
+		  $theValue = ($theValue != "") ? floatval($theValue)  : "NULL";
+		  break;
+		case "date":
+		  $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+		  break;
+		case "defined":
+		  $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+		  break;
+	   } // fin du switch
+	  return $theValue;
+	} // fin de la fonction
+} // fin du if
+
+$message=""; 
+$afficheformulaire="oui";
+
+if ((isset($_POST["Envoyer"])) && ($_POST["Envoyer"] == "Envoyer")) 
+ { // Clic sur bouton envoyer
+   // Vérifier que les champs sont remplis
+  $ok="oui";
+  if ($_POST['nom']=="") 
+   {
+    $message="Oups ... Vous avez oubli&eacute; de nous donner votre nom. Si vous voulez rester anonyme, donnez un pseudo pour pouvoir vous identifier";
+    $ok="non";
+   }
+  if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+   {
+   }
+     else  
+   {
+    $message="Il y a une erreur dans votre email ";
+    $ok="non";
+   } 
+  if ($_POST['civilite']=="??")
+   {
+    $message='Vous avez oubli&eacute; de d&eacute;finir la civilit&eacute;.';
+    $ok="non";
+   }
+  if ($ok=="oui")
+   { 
+	   $insertSQL = sprintf("INSERT INTO devis (ID, Nom, Prenom, Email, Tel, Message) VALUES (%s, %s, %s, %s, %s, %s)",
+						   GetSQLValueString($_POST['ID'], "int"),
+						   GetSQLValueString($_POST['nom'], "text"),
+						   GetSQLValueString($_POST['prenom'], "text"),
+						   GetSQLValueString($_POST['email'], "text"),
+						   GetSQLValueString($_POST['tel'], "text"),
+						   GetSQLValueString($_POST['descriptif'], "text"));
+	   $Result1 = mysqli_query($DevWebPeinture_db,$insertSQL);
+	   $message=" Votre devis nous a bien &eacute;t&eacute; transmis. Vous aurez de nos nouvelles tr&eacute;s bient&ocirc;t !";
+	   $afficheformulaire="non";
+   }
+ }
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -8,28 +85,56 @@
 
 <body>
 <div class="formulaire" id="formulaire">
-<table width="200" border="1">
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
+<?php if ($afficheformulaire=="oui") { ?>
+	<form action="" method="post" enctype="application/x-www-form-urlencoded" name="demande_devis">
+		<table width="200" border="0" align="center" class="formulaire">
+		  <tr>
+			<td colspan="2" align="left" valign="middle"><?php echo $message; ?></td>
+		  </tr>
+		  <tr>
+			<td align="right" valign="middle">Civilit&eacute;:</td>
+			<td align="left" valign="middle" nowrap="nowrap">
+			  <select name="civilite" id="civilite">
+			   <option value="??" <?php if ((!isset($_POST['civilite'])) or ($_POST['civilite']=="??")) {echo 'selected="selected"';}?>>??</option>
+			   <option value="Madame" <?php if ((isset($_POST['civilite'])) and($_POST['civilite']=="Madame")) {echo 'selected="selected"';}?>>Madame</option>
+			   <option value="Mademoiselle" <?php if ((isset($_POST['civilite'])) and($_POST['civilite']=="Mademoiselle")) {echo 'selected="selected"';}?>>Mademoiselle</option>
+			   <option value="Monsieur" <?php if ((isset($_POST['civilite'])) and($_POST['civilite']=="Monsieur")) {echo 'selected="selected"';}?>>Monsieur</option>
+			  </select>
+			</td>
+		  </tr>
+		
+		  <tr>
+			<td align="right" valign="middle">NOM:</td>
+			<td align="left" valign="middle" nowrap="nowrap"><input name="nom" type="text" />&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td align="right" valign="middle">Pr&eacute;nom:</td>
+			<td align="left" valign="middle" nowrap="nowrap"><input name="prenom" type="text" />&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td align="right" valign="middle">Email:</td>
+			<td align="left" valign="middle" nowrap="nowrap"><input name="email" type="text" />&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td align="right" valign="middle">T&eacute;l&eacute;phone:</td>
+			<td align="left" valign="middle" nowrap="nowrap"><input name="tel" type="text" />&nbsp;</td>
+		  </tr>
+		  <tr>
+			<td align="right" valign="middle">Descriptif des travaux: </td>
+			<td align="left" valign="middle"><textarea name="descriptif" cols="" rows=""></textarea>&nbsp;</td>
+		  </tr>
+  		  <tr>
+			<td colspan="2" align="right" valign="middle"><input name="Envoyer" type="submit" value="Envoyer" id="Envoyer" /></td>
+		  </tr>
+  		  <tr>
+			<td colspan="2" align="right" valign="middle"><input type="hidden" name="ID" id="ID" /></td>
+		  </tr>
+	  </table>
+  </form>
+  <?php
+    } // fin du if
+	else {echo $message;}
+  ?>	
 </div>
 </body>
 </html>
